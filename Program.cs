b65e1071c46ba94322dfbd22e9b90ca9f3ea1cb0
Andreas Teizel <andreas.teizel@posteo.de>
2019-12-19 23:59:22 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLgtm452
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test LGTM Dotnet Framework 4.5.2");
+            Console.ReadLine();
+        }
+    }
+}
